2f0099fc42d99858319bf0bd87bc9777982d731f
unknown <woodnlowkey>
2022-01-25 03:13:29 +0900

    'existence_set'

--- api_user/models.py
+++ api_user/models.py
@@ -3,35 +3,41 @@ from django.db import models
 class Bread(models.Model):
     name = models.CharField(max_length=64)
     stock = models.PositiveIntegerField(default=5)
-    price = models.PositiveIntegerField(default=50)
+    price = models.PositiveIntegerField()
+    # 기본값이 적용되지 않는 이슈?
+    existence = models.BooleanField(default=True)
     class Meta:
         db_table = "Bread"
 
 class Topping(models.Model):
     name = models.CharField(max_length=64)
     stock = models.PositiveIntegerField(default=5)
-    price = models.PositiveIntegerField(default=50)
+    price = models.PositiveIntegerField()
+    existence = models.BooleanField(default=True)
     class Meta:
         db_table = "Topping"
 
 class Cheese(models.Model):
     name = models.CharField(max_length=64)
     stock = models.PositiveIntegerField(default=5)
-    price = models.PositiveIntegerField(default=50)
+    price = models.PositiveIntegerField()
+    existence = models.BooleanField(default=True)
     class Meta:
         db_table = "Cheese"
 
 class Sauce(models.Model):
     name = models.CharField(max_length=64)
     stock = models.PositiveIntegerField(default=5)
-    price = models.PositiveIntegerField(default=50)
+    price = models.PositiveIntegerField()
+    existence = models.BooleanField(default=True)
     class Meta:
         db_table = "Sauce"
 
 class Sandwich(models.Model):
-    bread = models.ForeignKey(Bread, on_delete=models.DO_NOTHING, db_constraint=False)
-    topping = models.ForeignKey(Topping, on_delete=models.DO_NOTHING, db_constraint=False)
-    cheese = models.ForeignKey(Cheese, on_delete=models.DO_NOTHING, db_constraint=False)
-    sauce = models.ForeignKey(Sauce, on_delete=models.DO_NOTHING, db_constraint=False)
+    bread = models.ForeignKey(Bread, on_delete=models.CASCADE)
+    topping = models.ForeignKey(Topping, on_delete=models.CASCADE)
+    cheese = models.ForeignKey(Cheese, on_delete=models.CASCADE)
+    sauce = models.ForeignKey(Sauce, on_delete=models.CASCADE)
+    existence = models.BooleanField(default=True)
     class Meta:
         db_table = "Sandwich"
--- api_user/views.py
+++ api_user/views.py
@@ -9,52 +9,64 @@ from django.db.models import Q
 class BreadView(APIView):
     # 'bread/' 로 'post' 하는 경우 = 빵을 추가합니다.
     def post(self, request):
-        Bread_serializer = BreadSerializer(data=request.data) # Request의 data를 BreadSerializer로 변환
+        bread_serializer = BreadSerializer(data=request.data) # Request의 data를 BreadSerializer로 변환
         # 유효한 요청을 확인
-        if Bread_serializer.is_valid():
-            Bread_serializer.save() # BreadSerializer의 유효성 검사를 한 뒤 DB에 저장
-            return Response(Bread_serializer.data, status=status.HTTP_201_CREATED) # client에게 JSON response 전달
+        if bread_serializer.is_valid():
+            bread_serializer.save() # BreadSerializer의 유효성 검사를 한 뒤 DB에 저장
+            return Response(bread_serializer.data, status=status.HTTP_201_CREATED) # client에게 JSON response 전달
         else:
-            return Response(Bread_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(bread_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     # 'bread/' 로 'get' 하는 경우 = 빵 목록을 조회합니다.
     # 'bread/bread_id' 로 'get' 하는 경우 = 빵을 조회합니다.
     def get(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('bread_id') is None:
             bread_queryset = Bread.objects.all() # 모든 bread의 정보를 불러온다.
+            bread_queryset = bread_queryset.filter(Q(existence=True)).distinct()
             bread_queryset_serializer = BreadSerializer(bread_queryset, many=True)
             return Response(bread_queryset_serializer.data, status=status.HTTP_200_OK)
         else:
             bread_id = kwargs.get('bread_id')
-            Bread_serializer = BreadSerializer(Bread.objects.get(id=bread_id)) # id에 해당하는 bread의 정보를 불러온다.
-            return Response(Bread_serializer.data, status=status.HTTP_200_OK)
+            bread_queryset = Bread.objects.get(id=bread_id)
+            if bread_queryset.existence:
+                bread_serializer = BreadSerializer(bread_queryset) # id에 해당하는 bread의 정보를 불러온다.
+                return Response(bread_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'bread/bread_id' 로 'put' 하는 경우 = 빵을 수정합니다.
     def put(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('bread_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             bread_id = kwargs.get('bread_id')
             bread_object = Bread.objects.get(id=bread_id)
-            update_bread_serializer = BreadSerializer(bread_object, data=request.data)
-            # 유효한 요청을 확인
-            if update_bread_serializer.is_valid():
-                update_bread_serializer.save()
-                return Response(update_bread_serializer.data, status=status.HTTP_200_OK)
+            if bread_object.existence:
+                update_bread_serializer = BreadSerializer(bread_object, data=request.data)
+                # 유효한 요청을 확인
+                if update_bread_serializer.is_valid():
+                    update_bread_serializer.save()
+                    return Response(update_bread_serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response('Invalid Request', status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'bread/bread_id' 로 'delete' 하는 경우 = 빵을 삭제합니다.
     def delete(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('bread_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             bread_id = kwargs.get('bread_id')
             bread_object = Bread.objects.get(id=bread_id)
-            bread_name = bread_object.name
-            bread_object.delete()
-            # 삭제한 빵의 이름을 알림
-            return Response(f"{bread_name} removed", status=status.HTTP_200_OK)
+            if bread_object.existence:
+                bread_name = bread_object.name
+                bread_object.existence = False
+                bread_object.save()
+                # 삭제한 빵의 이름을 알림
+                return Response(f"{bread_name} has been deleted", status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
 
 class ToppingView(APIView):
     # 'topping/' 로 'post' 하는 경우 = 토핑을 추가합니다.
@@ -72,39 +84,51 @@ class ToppingView(APIView):
         # id가 요청에 있는 지 확인
         if kwargs.get('topping_id') is None:
             topping_queryset = Topping.objects.all() # 모든 topping의 정보를 불러온다.
+            topping_queryset = topping_queryset.filter(Q(existence=True)).distinct()
             topping_queryset_serializer = ToppingSerializer(topping_queryset, many=True)
             return Response(topping_queryset_serializer.data, status=status.HTTP_200_OK)
         else:
             topping_id = kwargs.get('topping_id')
-            topping_serializer = ToppingSerializer(Topping.objects.get(id=topping_id)) # id에 해당하는 topping의 정보를 불러온다.
-            return Response(topping_serializer.data, status=status.HTTP_200_OK)
+            topping_queryset = Topping.objects.get(id=topping_id)
+            if topping_queryset.existence:
+                topping_serializer = ToppingSerializer(topping_queryset) # id에 해당하는 topping의 정보를 불러온다.
+                return Response(topping_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'topping/topping_id' 로 'put' 하는 경우 = 토핑을 수정합니다.
     def put(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('topping_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             topping_id = kwargs.get('topping_id')
             topping_object = Topping.objects.get(id=topping_id)
-            update_topping_serializer = ToppingSerializer(topping_object, data=request.data)
-            # 유효한 요청을 확인
-            if update_topping_serializer.is_valid():
-                update_topping_serializer.save()
-                return Response(update_topping_serializer.data, status=status.HTTP_200_OK)
+            if topping_object.existence:
+                update_topping_serializer = ToppingSerializer(topping_object, data=request.data)
+                # 유효한 요청을 확인
+                if update_topping_serializer.is_valid():
+                    update_topping_serializer.save()
+                    return Response(update_topping_serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response('Invalid Request', status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'topping/topping_id' 로 'delete' 하는 경우 = 토핑을 삭제합니다.
     def delete(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('topping_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             topping_id = kwargs.get('topping_id')
             topping_object = Topping.objects.get(id=topping_id)
-            topping_name = topping_object.name
-            topping_object.delete()
-            # 삭제한 토핑의 이름을 알림
-            return Response(f"{topping_name} removed", status=status.HTTP_200_OK)
+            if topping_object.existence:
+                topping_name = topping_object.name
+                topping_object.existence = False
+                topping_object.save()
+                # 삭제한 토핑의 이름을 알림
+                return Response(f"{topping_name} has been deleted", status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
 
 class CheeseView(APIView):
     # 'cheese/' 로 'post' 하는 경우 = 치즈를 추가합니다.
@@ -122,39 +146,51 @@ class CheeseView(APIView):
         # id가 요청에 있는 지 확인
         if kwargs.get('cheese_id') is None:
             cheese_queryset = Cheese.objects.all() # 모든 cheese의 정보를 불러온다.
+            cheese_queryset = cheese_queryset.filter(Q(existence=True)).distinct()
             cheese_queryset_serializer = CheeseSerializer(cheese_queryset, many=True)
             return Response(cheese_queryset_serializer.data, status=status.HTTP_200_OK)
         else:
             cheese_id = kwargs.get('cheese_id')
-            cheese_serializer = CheeseSerializer(Cheese.objects.get(id=cheese_id)) # id에 해당하는 cheese의 정보를 불러온다.
-            return Response(cheese_serializer.data, status=status.HTTP_200_OK)
+            cheese_queryset = Cheese.objects.get(id=cheese_id)
+            if cheese_queryset.existence:
+                cheese_serializer = CheeseSerializer(cheese_queryset) # id에 해당하는 cheese의 정보를 불러온다.
+                return Response(cheese_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'cheese/cheese_id' 로 'put' 하는 경우 = 치즈를 수정합니다.
     def put(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('cheese_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             cheese_id = kwargs.get('cheese_id')
             cheese_object = Cheese.objects.get(id=cheese_id)
-            update_cheese_serializer = CheeseSerializer(cheese_object, data=request.data)
-            # 유효한 요청을 확인
-            if update_cheese_serializer.is_valid():
-                update_cheese_serializer.save()
-                return Response(update_cheese_serializer.data, status=status.HTTP_200_OK)
+            if cheese_object.existence:
+                update_cheese_serializer = CheeseSerializer(cheese_object, data=request.data)
+                # 유효한 요청을 확인
+                if update_cheese_serializer.is_valid():
+                    update_cheese_serializer.save()
+                    return Response(update_cheese_serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response('Invalid Request', status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'cheese/cheese_id' 로 'delete' 하는 경우 = 치즈를 삭제합니다.
     def delete(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('cheese_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             cheese_id = kwargs.get('cheese_id')
             cheese_object = Cheese.objects.get(id=cheese_id)
-            cheese_name = cheese_object.name
-            cheese_object.delete()
-            # 삭제한 치즈의 이름을 알림
-            return Response(f"{cheese_name} removed", status=status.HTTP_200_OK)
+            if cheese_object.existence:
+                cheese_name = cheese_object.name
+                cheese_object.existence = False
+                cheese_object.save()
+                # 삭제한 치즈의 이름을 알림
+                return Response(f"{cheese_name} has been deleted", status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
 
 class SauceView(APIView):
     # 'sauce/' 로 'post' 하는 경우 = 소스를 추가합니다.
@@ -172,39 +208,51 @@ class SauceView(APIView):
         # id가 요청에 있는 지 확인
         if kwargs.get('sauce_id') is None:
             sauce_queryset = Sauce.objects.all() # 모든 sauce의 정보를 불러온다.
+            sauce_queryset = sauce_queryset.filter(Q(existence=True)).distinct()
             sauce_queryset_serializer = SauceSerializer(sauce_queryset, many=True)
             return Response(sauce_queryset_serializer.data, status=status.HTTP_200_OK)
         else:
             sauce_id = kwargs.get('sauce_id')
-            sauce_serializer = SauceSerializer(Sauce.objects.get(id=sauce_id)) # id에 해당하는 sauce의 정보를 불러온다.
-            return Response(sauce_serializer.data, status=status.HTTP_200_OK)
+            sauce_queryset = Sauce.objects.get(id=sauce_id)
+            if sauce_queryset.existence:
+                sauce_serializer = SauceSerializer(sauce_queryset) # id에 해당하는 sauce의 정보를 불러온다.
+                return Response(sauce_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'sauce/sauce_id' 로 'put' 하는 경우 = 소스를 수정합니다.
     def put(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('sauce_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             sauce_id = kwargs.get('sauce_id')
             sauce_object = Sauce.objects.get(id=sauce_id)
-            update_sauce_serializer = SauceSerializer(sauce_object, data=request.data)
-            # 유효한 요청을 확인
-            if update_sauce_serializer.is_valid():
-                update_sauce_serializer.save()
-                return Response(update_sauce_serializer.data, status=status.HTTP_200_OK)
+            if sauce_object.existence:
+                update_sauce_serializer = SauceSerializer(sauce_object, data=request.data)
+                # 유효한 요청을 확인
+                if update_sauce_serializer.is_valid():
+                    update_sauce_serializer.save()
+                    return Response(update_sauce_serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response('Invalid Request', status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
     # 'sauce/sauce_id' 로 'delete' 하는 경우 = 소스를 삭제합니다.
     def delete(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('sauce_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         else:
             sauce_id = kwargs.get('sauce_id')
             sauce_object = Sauce.objects.get(id=sauce_id)
-            sauce_name = sauce_object.name
-            sauce_object.delete()
-            # 삭제한 소스의 이름을 알림
-            return Response(f"{sauce_name} removed", status=status.HTTP_200_OK)
+            if sauce_object.existence:
+                sauce_name = sauce_object.name
+                sauce_object.existence = False
+                sauce_object.save()
+                # 삭제한 소스의 이름을 알림
+                return Response(f"{sauce_name} has been deleted", status=status.HTTP_200_OK)
+            else:
+                return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
 
 # input = list[QueryObject, QueryObject, ...]
 # ouput = List[None] or list[str, str, ...]
@@ -243,15 +291,18 @@ class SandwichView(APIView):
         topping_object = Topping.objects.get(id=ingredients['topping'])
         cheese_object = Cheese.objects.get(id=ingredients['cheese'])
         sauce_object = Sauce.objects.get(id=ingredients['sauce'])
-        ingredient_objects = [
-            bread_object, topping_object, 
-            cheese_object, sauce_object
-            ]
+        if bread_object.existence and topping_object.existence and cheese_object.existence and sauce_object.existence:
+            ingredient_objects = [
+                bread_object, topping_object, 
+                cheese_object, sauce_object
+                ]
+        else:
+            return Response('Deleted Data', status=status.HTTP_400_BAD_REQUEST)
         # 별도의 함수를 통해 없는 재료가 있는 지 확인
         check_stock = check_ingredients(ingredient_objects)
         # 없는 재료가 있다면 알림
         if check_stock:
-            return Response(f"no more {check_stock}", status=status.HTTP_400_BAD_REQUEST)
+            return Response(f"No more {check_stock}", status=status.HTTP_400_BAD_REQUEST)
         # 유효한 요청을 확인
         elif sandwich_serializer.is_valid():
             sandwich_serializer.save() # SandwichSerializer의 유효성 검사를 한 뒤 DB에 저장
@@ -268,6 +319,7 @@ class SandwichView(APIView):
         # id가 요청에 있는 지 확인
         if kwargs.get('sandwich_id') is None:
             sandwich_queryset = Sandwich.objects.all() # 모든 sandwich의 정보를 불러온다.
+            sandwich_queryset = sandwich_queryset.filter(Q(existence=True)).distinct()
             # 검색 조건이 요청에 있는 지 확인
             kw_list = ('bread', 'topping', 'cheese', 'sauce', 'price-upper', 'price-lower')
             if kwargs.get('sandwich_kw') in kw_list and kwargs.get('kw_kn'):
@@ -285,13 +337,15 @@ class SandwichView(APIView):
         else:
             sandwich_id = kwargs.get('sandwich_id') # id에 해당하는 sandwich의 정보를 불러온다.
             sandwich = Sandwich.objects.get(id=sandwich_id)
+            if not sandwich.existence:
+                sandwich_id = f'{sandwich_id}-Deleted Sandwich'
             # 상세 정보 가져오기
             bread_object = Bread.objects.get(id=sandwich.bread_id)
             topping_object = Topping.objects.get(id=sandwich.topping_id)
             cheese_object = Cheese.objects.get(id=sandwich.cheese_id)
             sauce_object = Sauce.objects.get(id=sandwich.sauce_id)
             # 빵, 토핑, 치즈, 소스의 이름, 재고, 가격 정보 제공
-            return Response({sandwich.id:{
+            return Response({sandwich_id:{
                     'bread':{'name':bread_object.name, 'stock':bread_object.stock, 'price':bread_object.price},
                     'topping':{'name':topping_object.name, 'stock':topping_object.stock, 'price':topping_object.price}, 
                     'cheese':{'name':cheese_object.name, 'stock':cheese_object.stock, 'price':cheese_object.price}, 
@@ -301,7 +355,7 @@ class SandwichView(APIView):
     def delete(self, request, **kwargs):
         # id가 요청에 있는 지 확인
         if kwargs.get('sandwich_id') is None:
-            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         # id가 요청에 있는 경우
         else:
             sandwich_id = kwargs.get('sandwich_id')
@@ -324,6 +378,7 @@ class SandwichView(APIView):
                 # 재료 되돌리기
                 ingredient.stock += 1
                 ingredient.save()
-            sandwich_object.delete()
+            sandwich_object.existence = False
+            sandwich_object.save()
             # 삭제한 샌드위치의 재료 목록과 총 가격
             return Response({sandwich_id:{'total ingredients':total_ingredients, 'total price':total_price}}, status=status.HTTP_200_OK)
